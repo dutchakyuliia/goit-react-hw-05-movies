1b33ba45788013b486082ef79851362888c83455
Dutchak Yuliia <dutchakyuliia@gmail.com>
2023-06-03 16:15:49 +0200

    make page movie dateils

--- src/pages/MovieDetails/MovieDateils.jsx
+++ src/pages/MovieDetails/MovieDateils.jsx
@@ -1,6 +1,44 @@
-import React from "react"
+import Loader from "components/Loader/Loader";
+import MovieCard from "components/MovieCard/MovieCard";
+import React, { Suspense, useEffect, useState } from "react"
+import { Link, Outlet, useLocation, useParams } from "react-router-dom";
+import { fetchDetailsMovies } from "services/api";
 const MovieDetails = () => {
-return <h1>Movie Details</h1>
+ const { movieId } = useParams();
+  const location = useLocation();
+  const [selectedMovie, setSelectedMovie] = useState({});
+
+ 
+  useEffect(() => {
+    const fetchSelectedMovie = async movieId => {
+      try {
+        const movieData = await fetchDetailsMovies(movieId);
+        setSelectedMovie(movieData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchSelectedMovie(movieId);
+  }, [movieId]);
+
+  return (
+    <main>
+      <div>
+        <Link to={location?.state?.from ?? '/'}>
+          <button type="button">
+            Go back
+          </button>
+        </Link>
+
+        <MovieCard movie={selectedMovie} />
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </div>
+    </main>
+  );
+
 }
 
 export default MovieDetails
