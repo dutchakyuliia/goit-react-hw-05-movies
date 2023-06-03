a213314626c8b846b5d7ea6ad64fa4f1c1d66fd3
Dutchak Yuliia <dutchakyuliia@gmail.com>
2023-06-03 16:15:17 +0200

    make movie page

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,16 +1,59 @@
 import Search from 'components/Search/Search';
-import { React, useEffect, useState } from 'react';
-import * as api from 'services/api';
+import { useEffect, useState } from 'react';
+import { useSearchParams, useLocation, Link } from 'react-router-dom';
+import { fetchSearchMovies } from 'services/api';
+import { toast } from 'react-hot-toast';
 
 const Movies = () => {
-    const [movies, setMovies] = useState([]);
+   const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+
+  useEffect(() => {
+    const query = searchParams.get('query') ?? ''; 
+    if (!query) return;
+
+    const getMovie = async () => {
+      try {
+        const { results } = await fetchSearchMovies(query);
+
+        if (results.length === 0) {
+          toast.dismiss();
+          toast.error('No movies found');
+          setMovies([]); 
+        } else {
+          setMovies(results);
+        }
+      } catch (error) {
+        toast.error(error.message);
+        setMovies([]);
+      }
+    };
+
+      getMovie();
+  }, [searchParams]);
 
   
+  const handleSubmit = query => {
+    setSearchParams({ query }); 
+  };
+
   return (
-    <div>
-      <h1>Movies</h1>
-      <Search></Search>
-    </div>
+    <main>
+      <div>
+        <h2>Movies</h2>
+        <Search onSubmit={handleSubmit} /> 
+        <ul>
+          {movies.map(movie => (
+            <li key={movie.id}>
+              <Link to={`/movies/${movie.id}`} state={{ from: location }}>
+                {movie.title|| movie.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
   );
 };
 
